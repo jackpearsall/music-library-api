fb95e8487f16e871ca2cd9da65d6452e5626459f
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-06-11 18:31:41 +0100

    Finished and ready for merge

--- src/app.js
+++ src/app.js
@@ -3,14 +3,9 @@ const express = require('express');
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
-
 const app = express();
 
 app.use(express.json());
-
-// app.get('/hello', (req, res) => {
-//   res.status(200).json({ message: 'Hello World!' });
-// });
 
 app.use('/artists', artistRouter);
 app.use('/artists', albumRouter);
